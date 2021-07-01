4fa7ef35f3fca275d622013b21bc42f4e289415e
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2021-07-01 14:42:39 -0500

    I start the project..

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin') 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        open: true,
+        hot: true,
+        port: 3000
+    },
+    mode: 'development',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: { 
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.html$/,
+                use: 'html-loader'
+            },
+            {
+                test: /\.(s*)css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+              test: /\.(png|gif|jpg)$/,
+              use: [
+                {
+                  'loader': 'file-loader',
+                  options: {
+                    name: 'assets/[hash].[ext]'
+                  }
+                }
+              ]
+            }
+            
+        ]
+    },
+    plugins: [ 
+        new HtmlWebPackPlugin({
+            template: 'public/index.html',
+            filename: 'index.html'
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].css'
+        })
+    ]
+}
