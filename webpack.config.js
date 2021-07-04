4430a0754d6fc3cfe0ee47d160424896bad84517
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2021-07-04 13:13:54 -0500

    Update devServer Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -52,6 +52,10 @@ module.exports = {
             
         ]
     },
+    devServer: {
+        historyApiFallback: true
+    },
+    
     plugins: [ 
         new HtmlWebPackPlugin({
             template: 'public/index.html',
